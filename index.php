d337d562a3f1d8902b9f3dc9268489da012ab1ab
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-08-31 21:28:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>SITE RESPONSIVO</title>
+</head>
+<body>
+    <section id="topo">
+        <div class="banner">
+            <img src="images/banner-full.png" width="100%" alt="">
+        </div>
+    </section>
+    <section id="conteudo">
+        <div class="noticia">
+
+        </div>
+        <div class="noticia">
+
+        </div>
+
+    </section>
+    <footer id="rodape">
+
+    </footer>
+</body>
+</html>
